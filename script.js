79a90b6d48853d23ab209b40c4dceb2a8aa4b1ea
Matrix <ibrahimoyeniyi21@gmail.com>
2024-08-31 13:28:25 +0100

    Completed Project

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let clickedIcon = document.getElementsByClassName("share-icon");
+let clickedIcon = document.querySelector(".icon-div");
 
-let expandedView = document.getElementsByClassName("share-expand");
+let expandedView = document.querySelector(".share-expand");
 
+let closeExpand = document.querySelector(".i-span");
+
+let secondShare = document.querySelector(".i-span_two")
 
 clickedIcon.addEventListener("click", function(){
     expandedView.classList.toggle("active")
+
+    if (expandedView.classList.contains("active")){
+        clickedIcon.style.backgroundColor = "#48556a"
+        secondShare.style.color = "#fff"
+    }else{
+        clickedIcon.style.backgroundColor = "#ecf2f8"
+        secondShare.style.color = "#48556a"
+    }
+})
+
+closeExpand.addEventListener("click", function(){
+    if (expandedView.classList.contains("active")){
+        expandedView.classList.remove("active")
+    }else{
+        expandedView.classList.add("active")
+    }
 })
